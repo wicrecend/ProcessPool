0fe166370244c3b6c1afbe333d4c918c675311c6
JumpingYang001 <v-zhenpy@microsoft.com>
2017-04-19 20:04:58 -0700

    Add ProcessPool project and README.md

---
+++ ProcessPool/ProcessPool/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace ProcessPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string commandsFile = ConfigurationManager.AppSettings["commandsFile"];
+            string processMaxNumber = ConfigurationManager.AppSettings["processMaxNumber"];
+            int maxNumber = Convert.ToInt32(processMaxNumber);
+            // Read txt file from commands.txt
+            // foreach every command, and start process with maxNumber number processes according to the command,
+            // then waiting the exist process exit and start new process to maxNumber number processes until all commands in commands are done.
+            // then show the final result: "All Pass", "Partial Pass, command "***" need to retry", "All Failed".
+            // if show the text with Green, Yellow, Red color on the console, it will be better.
+            Console.WriteLine("Please press Enter key to exit the program.");
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ProcessPool
 Start serveral process which process commands read from txt file
+
+# Functions Implementation
+
+// Read txt file from commands.txt
+
+// foreach every command, and start process with maxNumber number processes according to the command,
+
+// then waiting the exist process exit and start new process to maxNumber number processes until all commands in commands are done.
+ 
+ // then show the final result: "All Pass", "Partial Pass, command "***" need to retry", "All Failed".
+
+ // if show the text with Green, Yellow, Red color on the console, it will be better.
